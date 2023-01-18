7296c2a044591fd94a7ec36895868a4cf3f1c4c0
Ian Wijma <ian@wij.ma>
2023-01-18 19:55:25 +1100

    Clean

--- apps/desktop/src/app/constants.ts
+++
@@ -1,7 +0,0 @@
-export const rendererAppPort = 4200;
-export const rendererAppName = 'desktop-frontend';
-export const electronAppName = 'Thermal Printer Notifications';
-export const defaultPort = 1337;
-export const disconnectedString = 'thermal-printer-notification-disconnected';
-export const connectedString = 'thermal-printer-notification-connected';
-export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
